ab4d0ee313b7e65cf92738bc1b92ca732f937af8
Aviraj Gite <giteaviraj155@gmail.com>
2026-04-03 17:23:06 +0530

    DOM first mini project

---
+++ color switcher/script.js
@@ -0,0 +1,8 @@
+let colorInput = document.querySelector("#colorInput")
+let randombtn = document.querySelectorAll('.randombtn');
+let applyBtn = document.querySelectorAll('.applyBtn');
+let currentColorValue = document.querySelector('.currentColorValue')
+
+console.log(randombtn, colorInput, applyBtn, currentColorValue)
+
+randombtn
