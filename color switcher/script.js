6c4a0ad59a23c1b74bed96cee7f197f88a44da58
Aviraj Gite <giteaviraj155@gmail.com>
2026-04-05 09:33:34 +0530

    added new logic in color switcher

--- color switcher/script.js
+++ color switcher/script.js
@@ -1,8 +1,50 @@
 let colorInput = document.querySelector("#colorInput")
-let randombtn = document.querySelectorAll('.randombtn');
-let applyBtn = document.querySelectorAll('.applyBtn');
-let currentColorValue = document.querySelector('.currentColorValue')
+let randombtn = document.querySelector('.randombtn');
+let applyBtn = document.querySelector('.applyBtn');
+let currentColorValue = document.querySelector('.currentColorValue');
+let container = document.querySelector('.container')
 
-console.log(randombtn, colorInput, applyBtn, currentColorValue)
+
+const getRandomColor= () => {
+    return '#' + Math.floor(Math.random()* 16777215).toString(16);
+}
+
+const changeColor =(color)=>{
+  container.style.backgroundColor=color;
+}
+
+const handleRandomButtonClick =()=>{
+   let randomColor = getRandomColor();
+
+   changeColor(randomColor);
+   currentColorValue.textContent = randomColor;
+   console.log(randomColor)
+}
+
+colorInput.addEventListener("input",()=>{
+    changeColor(colorInput.value)
+})
+
+const handleApplyBtnClick = ()=>{
+    let color = colorInput.value
+
+    container.style.backgroundColor=color;
+    currentColorValue.textContent=color;
+    console.log(color)
+}
 
 
+
+randombtn.addEventListener('click',handleRandomButtonClick)
+
+
+applyBtn.addEventListener('click',handleApplyBtnClick)
+
+
+
+
+
+
+
+
+// changeColor('black')
